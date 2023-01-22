c942103bce2ca0ae2a4d1033812d000a056e82ea
chagasleandro <chagasleandro>
2023-01-22 18:38:22 -0300

    create folder components

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import './index.css';
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import Primeiro  from './components/Primeiro';
+import ComParamentro from './components/ComParamentro';
+
 ReactDom.render(
-    <h1>Olá React!</h1>,
+    <div>
+        <Primeiro />
+    <ComParamentro titulo="Esse é o titulo"
+    subtitulo="Esse é o subtitulo" />
+    </div>,
     document.getElementById("root")
 )
